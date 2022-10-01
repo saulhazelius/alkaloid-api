edcf4326f0e75efc14f133c098975785de2d21c8
saul <hazelius@gmail.com>
2022-09-30 23:58:28 -0500

    Add train with mlflow integration

---
+++ train/train.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from data.preprocessing import processed_data
+from xgboost import XGBClassifier
+from sklearn.model_selection import cross_val_score
+import mlflow
+from mlflow.xgboost import autolog 
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+MODEL_CONFIG_PATH = "../configs/model_config.json"
+
+def load_model_definition(model_path):
+    
+    with open(model_path) as w:
+        model_def = json.load(w)
+
+    return model_def
+
+
+def train(model_definition, X, y):
+
+    model_name = model_definition["model"]
+    params = model_definition["params"]
+    n_samples = len(X)
+
+    if model_name == "XGBClassifier":
+        
+        logging.info(f"Loaded model: {model_name}")
+        logging.info(f"Number of samples for training: {n_samples}")
+
+        autolog()
+        with mlflow.start_run():
+            
+            model = XGBClassifier(**params)
+            model.fit(X, y)
+
+    else:
+        logging.debug("Please verify model name and definition")
+
+if __name__ == '__main__':
+    
+    model = load_model_definition(MODEL_CONFIG_PATH)
+    X, y = processed_data
+    train(model, X, y)
