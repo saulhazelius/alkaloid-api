6f53167d144303f9de12249a11a5e215decba000
saul <hazelius@gmail.com>
2022-10-03 01:05:11 -0500

    Reformat scripts

--- project/predict.py
+++ project/predict.py
@@ -9,18 +9,22 @@ logger.setLevel(logging.INFO)
 
 MODEL_PATH = "../models/model.xgb"
 
+
 def prediction(model_path, inpt, max_len):
     """Predicts alkaloid or non-alkaloid existence for input arrays.
-       If input size > MAX_LEN automatically predicts non-alkaloid."""
+    If input size > MAX_LEN automatically predicts non-alkaloid."""
     if len(inpt[0]) > max_len:
 
         predicted_value = np.array([0])
-        logger.info("Number of 13C spectra greater than max spectra for alkaloids. Model not loaded for prediction.")
+        logger.info(
+            "Number of 13C spectra greater than max spectra for alkaloids."
+            "Model not loaded for prediction."
+        )
 
     else:
 
         xgb_model = XGBClassifier()
-        xgb_model.load_model(model_path) #Load model
+        xgb_model.load_model(model_path)  # Load model
         logger.info(f"Model for prediction: {xgb_model}")
         predicted_value = xgb_model.predict(inpt)
 
@@ -31,14 +35,12 @@ if __name__ == "__main__":
 
     max_len = max_len
     inpt = created_input
-    
+
     logger.info(f"Array of spectra: {inpt}")
 
     model_path = MODEL_PATH
-    
+
     predicted_value = prediction(model_path, inpt, max_len)
-    logger.info(f"Alkaloid prediction: {True if predicted_value == 1 else False}")
-
-
-
-
+    logger.info(
+        f"Alkaloid prediction: {True if predicted_value == 1 else False}"
+    )
